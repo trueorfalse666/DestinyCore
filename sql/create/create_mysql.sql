e81cccc6aa3de94ddd8d23b6de105f8786606dbf
wang radium <741814620@qq.com>
2026-04-24 23:23:18 +0800

    Create user and databases with privileges
    This script creates a user and multiple databases if they do not exist, and grants all privileges to the user on those databases. It also includes a command to flush privileges.

--- sql/create/create_mysql.sql
+++ sql/create/create_mysql.sql
@@ -1,17 +1,17 @@
-GRANT USAGE ON * . * TO 'trinity'@'localhost' IDENTIFIED BY 'trinity' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 ;
+-- 1. 创建用户（不存在才创建）
+CREATE USER IF NOT EXISTS 'trinity'@'localhost' IDENTIFIED BY 'trinity';
 
-CREATE DATABASE `world` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- 2. 创建数据库（不存在才创建，不会报错）
+CREATE DATABASE IF NOT EXISTS `world` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `characters` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `auth` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `hotfixes` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE DATABASE `characters` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- 3. 授权
+GRANT ALL PRIVILEGES ON `world`.* TO 'trinity'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `characters`.* TO 'trinity'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `auth`.* TO 'trinity'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `hotfixes`.* TO 'trinity'@'localhost' WITH GRANT OPTION;
 
-CREATE DATABASE `auth` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-CREATE DATABASE `hotfixes` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-GRANT ALL PRIVILEGES ON `world` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES ON `characters` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES ON `auth` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES ON `hotfixes` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
+-- 4. 刷新权限
+FLUSH PRIVILEGES;
