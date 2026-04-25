f96179550c2aa4935e5ac37e9eb2e761a89fef85
wang radium <741814620@qq.com>
2026-04-25 22:37:29 +0800

    Change user from 'trinity' to 'destiny' in SQL script

--- sql/create/create_mysql.sql
+++ sql/create/create_mysql.sql
@@ -1,5 +1,5 @@
 -- 1. 创建用户（不存在才创建）
-CREATE USER IF NOT EXISTS 'trinity'@'localhost' IDENTIFIED BY 'trinity';
+CREATE USER IF NOT EXISTS 'destiny'@'localhost' IDENTIFIED BY 'trinity';
 
 -- 2. 创建数据库（不存在才创建，不会报错）
 CREATE DATABASE IF NOT EXISTS `world` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
@@ -8,10 +8,10 @@ CREATE DATABASE IF NOT EXISTS `auth` DEFAULT CHARACTER SET utf8 COLLATE utf8_gen
 CREATE DATABASE IF NOT EXISTS `hotfixes` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- 3. 授权
-GRANT ALL PRIVILEGES ON `world`.* TO 'trinity'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON `characters`.* TO 'trinity'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON `auth`.* TO 'trinity'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON `hotfixes`.* TO 'trinity'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `world`.* TO 'destiny'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `characters`.* TO 'destiny'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `auth`.* TO 'destiny'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `hotfixes`.* TO 'destiny'@'localhost' WITH GRANT OPTION;
 
 -- 4. 刷新权限
 FLUSH PRIVILEGES;
