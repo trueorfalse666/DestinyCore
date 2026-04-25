63381c312bae46cbaa635f691d1733f40391f942
wang radium <741814620@qq.com>
2026-04-25 22:38:24 +0800

    Change user privileges and drop user 'trinity' to 'destiny'

--- sql/create/drop_mysql.sql
+++ sql/create/drop_mysql.sql
@@ -1,22 +1,22 @@
-REVOKE ALL PRIVILEGES ON * . * FROM 'trinity'@'localhost';
+REVOKE ALL PRIVILEGES ON * . * FROM 'destiny'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `world` . * FROM 'trinity'@'localhost';
+REVOKE ALL PRIVILEGES ON `world` . * FROM 'destiny'@'localhost';
 
-REVOKE GRANT OPTION ON `world` . * FROM 'trinity'@'localhost';
+REVOKE GRANT OPTION ON `world` . * FROM 'destiny'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `characters` . * FROM 'trinity'@'localhost';
+REVOKE ALL PRIVILEGES ON `characters` . * FROM 'destiny'@'localhost';
 
-REVOKE GRANT OPTION ON `characters` . * FROM 'trinity'@'localhost';
+REVOKE GRANT OPTION ON `characters` . * FROM 'destiny'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `auth` . * FROM 'trinity'@'localhost';
+REVOKE ALL PRIVILEGES ON `auth` . * FROM 'destiny'@'localhost';
 
-REVOKE GRANT OPTION ON `auth` . * FROM 'trinity'@'localhost';
+REVOKE GRANT OPTION ON `auth` . * FROM 'destiny'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `hotfixes` . * FROM 'trinity'@'localhost';
+REVOKE ALL PRIVILEGES ON `hotfixes` . * FROM 'destiny'@'localhost';
 
-REVOKE GRANT OPTION ON `hotfixes` . * FROM 'trinity'@'localhost';
+REVOKE GRANT OPTION ON `hotfixes` . * FROM 'destiny'@'localhost';
 
-DROP USER 'trinity'@'localhost';
+DROP USER 'destiny'@'localhost';
 
 DROP DATABASE IF EXISTS `world`;
 
